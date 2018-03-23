07dbd4ced92f4aa88481cfff51b015296fb1f9a1
Lucas Schmidt <lucas@Lucass-MacBook-Air-2.local>
2018-03-23 13:05:49 +0100

    rename

--- src/main/scala/de/htwg/se/twothousandfortyeight/YourGame.scala
+++ src/main/scala/de/htwg/se/twothousandfortyeight/YourGame.scala
@@ -1,6 +1,6 @@
-package de.htwg.se.yourgame
+package de.htwg.se.twothousandfortyeight
 
-import de.htwg.se.yourgame.model.Player
+import de.htwg.se.twothousandfortyeight.model.Player
 
 object YourGame {
   def main(args: Array[String]): Unit = {
