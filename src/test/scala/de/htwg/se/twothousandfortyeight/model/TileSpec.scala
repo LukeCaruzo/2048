21ef13fc3648f8875c901804cb2fd0d12ff87f7b
LukeCaruzo <schmidt.lucas@googlemail.com>
2018-04-22 10:35:20 +0200

    tests but not implemented yet

---
+++ src/test/scala/de/htwg/se/twothousandfortyeight/model/TileSpec.scala
@@ -0,0 +1,10 @@
+package de.htwg.se.twothousandfortyeight.model
+
+import org.junit.runner.RunWith
+import org.scalatest._
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class TileSpec extends WordSpec with Matchers {
+  fail("not implemented")
+}
