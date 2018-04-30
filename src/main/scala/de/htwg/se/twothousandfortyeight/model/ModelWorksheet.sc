faf2f7408b96c05ab11a0f098fd4fee324e97158
LukeCaruzo <schmidt.lucas@googlemail.com>
2018-04-30 10:49:18 +0200

    grid initial tests

--- src/main/scala/de/htwg/se/twothousandfortyeight/model/ModelWorksheet.sc
+++ src/main/scala/de/htwg/se/twothousandfortyeight/model/ModelWorksheet.sc
@@ -1,12 +1,6 @@
-case class Cell(x:Int, y:Int)
+import de.htwg.se.twothousandfortyeight.model.Grid
 
-val cell1 = Cell(4,5)
-cell1.x
-cell1.y
+var grid = new Grid()
 
-case class Field(cells: Array[Cell])
-
-val field1 = Field(Array.ofDim[Cell](1))
-field1.cells(0)=cell1
-field1.cells(0).x
-field1.cells(0).y
+grid.resetGrid
+grid.getAvailableSpace.toString
--- src/test/scala/de/htwg/se/twothousandfortyeight/model/GridSpec.scala
+++ src/test/scala/de/htwg/se/twothousandfortyeight/model/GridSpec.scala
@@ -6,5 +6,20 @@ import org.scalatest.junit.JUnitRunner
 
 @RunWith(classOf[JUnitRunner])
 class GridSpec extends WordSpec with Matchers {
-  //fail("not implemented")
+  "A Grid" when {
+    "new" should {
+      var grid = new Grid()
+      "have a initial tiles" in {
+        grid.tiles should be(new Array[Tile](16))
+      }
+      "have a isFull method" in {
+        grid.resetGrid
+        grid.isFull should be(false)
+      }
+      "have a getAvailableSpace method" in {
+        grid.resetGrid
+        grid.getAvailableSpace.toString should be("[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]")
+      }
+    }
+  }
 }
