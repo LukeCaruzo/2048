2fe98061331523a78d95a7eb72d23c20c5f74c6b
LukeCaruzo <schmidt.lucas@googlemail.com>
2018-05-08 14:00:30 +0200

    Tests for setSingleLine

--- src/test/scala/de/htwg/se/twothousandfortyeight/model/GridSpec.scala
+++ src/test/scala/de/htwg/se/twothousandfortyeight/model/GridSpec.scala
@@ -64,6 +64,9 @@ class GridSpec extends WordSpec with Matchers {
         grid.tiles = Array(new Tile(2), new Tile(4), new Tile(8), new Tile(16), new Tile(32), new Tile(64), new Tile(128), new Tile(256), new Tile(512), new Tile(1024), new Tile(2048), new Tile(4096), new Tile(64), new Tile(32), new Tile(16), new Tile(1024))
         grid.canBeMoved should be(false)
       }
+      "have a setSingleLine method" in {
+        grid.setSingleLine(1, Array(new Tile(0), new Tile(0), new Tile(0), new Tile(0))) should be()
+      }
     }
   }
 }
