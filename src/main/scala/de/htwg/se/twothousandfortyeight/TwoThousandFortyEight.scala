4dbfe2ba2b173dcb97314b7c797a4cdde48e4d28
LukeCaruzo <schmidt.lucas@googlemail.com>
2018-06-11 13:47:21 +0200

    Added name input

--- src/main/scala/de/htwg/se/twothousandfortyeight/TwoThousandFortyEight.scala
+++ src/main/scala/de/htwg/se/twothousandfortyeight/TwoThousandFortyEight.scala
@@ -1,18 +1,21 @@
 package de.htwg.se.twothousandfortyeight
 
+import java.util.Scanner
+
 import de.htwg.se.twothousandfortyeight.model.{Grid, Player, Score}
 import de.htwg.se.twothousandfortyeight.view.gui.GraphicalUserInterface
 //import de.htwg.se.twothousandfortyeight.view.tui.TextUserInterface
 
 object TwoThousandFortyEight {
-  val FIELD_SIZE = 4 // Tests are configured for 4
+  var FIELD_SIZE = 9 // Tests are configured for 4
   var win = false
   var lose = false
   var score = new Score
   var grid = new Grid
 
   def main(args: Array[String]): Unit = {
-    val student = new Player("Lucas/Bilal")
+    println("Enter your name: ")
+    val student = new Player(new Scanner(System.in).next())
 
     //new TextUserInterface(student)
     new GraphicalUserInterface(student)
