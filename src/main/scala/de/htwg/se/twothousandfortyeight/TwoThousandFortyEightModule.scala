3c11b9a17a480da3d87744443fc46f485eb4f95a
bilalra <bilalrachachi@gmail.com>
2018-06-19 15:01:50 +0200

    added tests, module and xml

--- src/main/scala/de/htwg/se/twothousandfortyeight/TwoThousandFortyEightModule.scala
+++ src/main/scala/de/htwg/se/twothousandfortyeight/TwoThousandFortyEightModule.scala
@@ -3,12 +3,15 @@ package de.htwg.se.twothousandfortyeight
 import com.google.inject.AbstractModule
 import de.htwg.se.twothousandfortyeight.model.fileIoModel.{FileIoTrait, fileIoXmlImpl}
 import de.htwg.se.twothousandfortyeight.model.gameModel.GameTrait
-import de.htwg.se.twothousandfortyeight.model.gameModel.gameBaseImpl.Game
+import de.htwg.se.twothousandfortyeight.model.gameModel.TileTrait
+
+import de.htwg.se.twothousandfortyeight.model.gameModel.gameBaseImpl
 import net.codingwell.scalaguice.ScalaModule;
 
 class TwoThousandFortyEightModule extends AbstractModule with ScalaModule {
   override def configure(): Unit = {
-    bind[GameTrait].to[Game]
+    bind[GameTrait].to[gameBaseImpl.Game]
+    bind[TileTrait].to[gameBaseImpl.Tile]
     bind[FileIoTrait].to[fileIoXmlImpl.FileIo]
   }
 }
--- src/main/scala/de/htwg/se/twothousandfortyeight/model/fileIoModel/fileIoXmlImpl/FileIo.scala
+++ src/main/scala/de/htwg/se/twothousandfortyeight/model/fileIoModel/fileIoXmlImpl/FileIo.scala
@@ -1,6 +1,6 @@
 package de.htwg.se.twothousandfortyeight.model.fileIoModel.fileIoXmlImpl
 
-import java.io.{BufferedWriter, File, FileWriter}
+import java.io._
 
 import de.htwg.se.twothousandfortyeight.model.fileIoModel.FileIoTrait
 import de.htwg.se.twothousandfortyeight.model.gameModel.GameTrait
@@ -10,14 +10,15 @@ import scala.io.Source
 class FileIo extends FileIoTrait {
   def save(filename: String, game: GameTrait): Unit = {
     val file = new File(filename + ".xml")
-    val bw = new BufferedWriter(new FileWriter(file))
-    bw.write("hello world")
+    val bw = new FileWriter(file)
+
+    bw.write("")
     bw.close()
   }
 
   def load(filename: String, game: GameTrait): Unit = {
     val source = Source.fromFile(filename + ".xml")
     val lines = try source.mkString finally source.close()
-    println(lines)
+
   }
 }
