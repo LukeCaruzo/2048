800b7fa596cb1085a7ef3be5a24a6c8d57a54427
bilalra <bilalrachachi@gmail.com>
2018-06-19 14:31:02 +0200

    added tests for fileIo

--- src/test/scala/de/htwg/se/twothousandfortyeight/model/FileIoSpec.scala
+++ src/test/scala/de/htwg/se/twothousandfortyeight/model/FileIoSpec.scala
@@ -1,5 +1,40 @@
 package de.htwg.se.twothousandfortyeight.model
 
-class FileIoSpec {
+import de.htwg.se.twothousandfortyeight.model.fileIoModel.fileIoJsonImpl
+import de.htwg.se.twothousandfortyeight.model.fileIoModel.fileIoXmlImpl
+import de.htwg.se.twothousandfortyeight.model.gameModel.GameTrait
+import de.htwg.se.twothousandfortyeight.model.gameModel.gameBaseImpl.Game
+import org.junit.runner.RunWith
+import org.scalatest.{Matchers, WordSpec}
+import org.scalatest.junit.JUnitRunner
 
+@RunWith(classOf[JUnitRunner])
+class FileIoSpec extends WordSpec with Matchers {
+  "FileIo" when {
+    "new" should {
+      val fileIoJson = new fileIoJsonImpl.FileIo
+      val fileIoXml = new fileIoXmlImpl.FileIo
+
+      val game = new Game
+      "have a save (json) " in {
+        game.win = true
+        fileIoJson.save("test.2048", game)
+      }
+      "have a load (json) method" in {
+        game.win = false
+        fileIoJson.load("test.2048", game)
+        game.win should be(true)
+      }
+      "have a save (xml) " in {
+        game.win = true
+        fileIoXml.save("test.2048", game)
+      }
+      "have a load (xml) method" in {
+        game.win = false
+        fileIoXml.load("test.2048", game)
+        game.win should be(true)
+      }
+    }
+  }
 }
+
