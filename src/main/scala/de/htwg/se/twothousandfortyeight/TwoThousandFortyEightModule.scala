eb044f4354cee17b64dcb64ba4de7701b72caa0a
LukeCaruzo <schmidt.lucas@googlemail.com>
2018-06-21 18:43:37 +0200

    Module

--- src/main/scala/de/htwg/se/twothousandfortyeight/TwoThousandFortyEightModule.scala
+++ src/main/scala/de/htwg/se/twothousandfortyeight/TwoThousandFortyEightModule.scala
@@ -1,7 +1,7 @@
 package de.htwg.se.twothousandfortyeight
 
 import com.google.inject.AbstractModule
-import de.htwg.se.twothousandfortyeight.model.fileIoModel.{FileIoTrait, fileIoXmlImpl}
+import de.htwg.se.twothousandfortyeight.model.fileIoModel.{FileIoTrait, fileIoXmlImpl, fileIoJsonImpl}
 import de.htwg.se.twothousandfortyeight.model.gameModel.GameTrait
 
 import de.htwg.se.twothousandfortyeight.model.gameModel.gameBaseImpl
@@ -10,6 +10,7 @@ import net.codingwell.scalaguice.ScalaModule
 class TwoThousandFortyEightModule extends AbstractModule with ScalaModule {
   override def configure(): Unit = {
     bind[GameTrait].to[gameBaseImpl.Game]
-    bind[FileIoTrait].to[fileIoXmlImpl.FileIo]
+    //bind[FileIoTrait].to[fileIoXmlImpl.FileIo]
+    bind[FileIoTrait].to[fileIoJsonImpl.FileIo]
   }
 }
