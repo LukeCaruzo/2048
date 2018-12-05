9ac06f7d2d450f40a91daa179a94c01273d775e6
LukeCaruzo <schmidt.lucas@googlemail.com>
2018-12-05 10:24:18 +0100

    workflow

--- src/main/scala/de/htwg/se/twothousandfortyeight/TwoThousandFortyEight.scala
+++ src/main/scala/de/htwg/se/twothousandfortyeight/TwoThousandFortyEight.scala
@@ -8,10 +8,9 @@ object TwoThousandFortyEight {
   val FIELD_SIZE = 4 // Tests are configured for 4
 
   def main(args: Array[String]): Unit = {
-    val student = "Lucas/Bilal"
     val game = new Game
     val turn = new Turn
 
-    new Tui(student, game, turn)
+    new Tui(game, turn)
   }
 }
--- src/main/scala/de/htwg/se/twothousandfortyeight/view/tui/Tui.scala
+++ src/main/scala/de/htwg/se/twothousandfortyeight/view/tui/Tui.scala
@@ -1,12 +1,67 @@
 package de.htwg.se.twothousandfortyeight.view.tui
 
+import java.awt.event.KeyEvent
+
 import de.htwg.se.twothousandfortyeight.controller.TurnTrait
+import de.htwg.se.twothousandfortyeight.controller.turnBaseImpl.Turn
 import de.htwg.se.twothousandfortyeight.model.gameModel.GameTrait
+import de.htwg.se.twothousandfortyeight.util.Utils
 
 import scala.swing.Reactor
+import scala.swing.event.KeyReleased
 
-class Tui(player: String, game: GameTrait, turn: TurnTrait) extends Reactor {
+class Tui(game: GameTrait, turn: TurnTrait) extends Reactor {
   listenTo(turn)
+
+  println("Hello. Game started!")
+  println("Used W A S D to move and R to reset and T to exit and Z to save and U to load and Q to undo.")
+  println()
+  println(game.grid.toString)
+  println("Your Score: " + game.score.toString)
+  println()
+
+  reactions += {
+    case event: KeyReleased => {
+      turn.makeTurn(game, Utils.processKey(event.key.id, 'x'), Math.random(), Math.random())
+
+      if (game.win) {
+        println("You won!")
+      } else if (game.lose) {
+        println("You lost!")
+      } else {
+        println(game.grid.toString)
+        println("Your Score: " + game.score.toString)
+        println()
+      }
+    }
+  }
+
+  //  def printTui(): Unit = {
+  //    if (game.win) {
+  //      println("You won!")
+  //    } else if (game.lose) {
+  //      println("You lost!")
+  //    } else {
+  //      println(game.grid.toString)
+  //      println("Your Score: " + game.score.toString)
+  //      println()
+  //    }
+  //  }
+
+  //  override def keyTyped(e: KeyEvent): Unit = {
+  //    val turn = new Turn
+  //    turn.makeTurn(game, Utils.processKey(e.getExtendedKeyCode, e.getKeyChar), Math.random(), Math.random())
+  //
+  //    if (game.win) {
+  //      println("You won!")
+  //    } else if (game.lose) {
+  //      println("You lost!")
+  //    } else {
+  //      println(game.grid.toString)
+  //      println("Your Score: " + game.score.toString)
+  //      println()
+  //    }
+  //  }
 
   //  reactions += {
   //    case event: GridSizeChanged => printTui
