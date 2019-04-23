aa10eee1d89363643ff7e49d338e3198e6466071
LukeCaruzo <schmidt.lucas@googlemail.com>
2019-04-23 09:10:47 +0200

    first prototype for actors

---
+++ src/main/scala/de/htwg/se/twothousandfortyeight/model/actorModel/Counter.scala
@@ -0,0 +1,7 @@
+package de.htwg.se.twothousandfortyeight.model.actorModel
+
+import akka.actor.Actor
+
+class Counter extends Actor {
+  override def receive: Receive = ???
+}
---
+++ src/main/scala/de/htwg/se/twothousandfortyeight/model/actorModel/Resolver.scala
@@ -0,0 +1,11 @@
+package de.htwg.se.twothousandfortyeight.model.actorModel
+
+import akka.actor.Actor
+
+object Resolver {
+
+}
+
+class Resolver extends Actor {
+  override def receive: Receive = ???
+}
