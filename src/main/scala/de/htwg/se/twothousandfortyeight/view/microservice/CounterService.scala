fa51235d28bd1ff1d7894a2af478b7911166fe71
LukeCaruzo <schmidt.lucas@googlemail.com>
2019-05-15 09:04:10 +0200

    counter

---
+++ src/main/scala/de/htwg/se/twothousandfortyeight/view/microservice/CounterService.scala
@@ -0,0 +1,33 @@
+package de.htwg.se.twothousandfortyeight.view.microservice
+
+import scala.io.StdIn
+
+object CounterServiceMain {
+  def main(args: Array[String]): Unit = {
+    val webserver = new CounterServiceServer(new CounterService)
+
+    println("Server online at http://localhost:8081/")
+    println("Press RETURN to stop...")
+
+    StdIn.readLine() // let it run until user presses return
+    webserver.unbind()
+  }
+}
+
+class CounterService {
+  var counter = 0
+
+  def inc(): Unit = {
+    counter += 1
+  }
+
+  def dec(): Unit = {
+    counter -= 1
+  }
+
+  override def toString: String = counter.toString
+
+  def toHtml: String = {
+    "<p>" + toString + "</p>"
+  }
+}
