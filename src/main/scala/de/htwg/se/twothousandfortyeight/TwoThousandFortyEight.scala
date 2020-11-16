56acf85df381f61f27942338f389a032bc444704
LukeCaruzo <schmidt.lucas@googlemail.com>
2020-11-16 12:50:34 +0100

    changed === to shouldBe

--- src/main/scala/de/htwg/se/twothousandfortyeight/TwoThousandFortyEight.scala
+++ src/main/scala/de/htwg/se/twothousandfortyeight/TwoThousandFortyEight.scala
@@ -1,5 +1,6 @@
 package de.htwg.se.twothousandfortyeight
 
+import de.htwg.se.twothousandfortyeight.controller.actorInternalDslImpl._
 import de.htwg.se.twothousandfortyeight.controller.turnBaseImpl.Turn
 import de.htwg.se.twothousandfortyeight.view.microservice.{CounterService, CounterServiceServer, HighScoreService, HighScoreServiceServer}
 import de.htwg.se.twothousandfortyeight.view.tui.{Rest, Tui}
@@ -8,11 +9,13 @@ object TwoThousandFortyEight {
   val FIELD_SIZE = 4 // Tests and Slick are configured for 4
 
   def main(args: Array[String]): Unit = {
-    val turn = new Turn
+    /*val turn = new Turn
 
     new Rest(turn)
     //new CounterServiceServer(new CounterService) // fix for docker
     //new HighScoreServiceServer(new HighScoreService) // fix for docker
-    new Tui(turn)
+    new Tui(turn)*/
+
+    Move(w,a,s,d,w,a,s,d,w,a,s,d,w,a,s,d,w,a,s,d,w,a,s,d,w,a,s,d,w,a,s,d,w,a,s,d,w,a,s,d,w,a,s,d,w,a,s,d,w,a,s,d,w,a,s,d,w,a,s,d,w,a,s,d,w,a,s,d,w,a,s,d,w,a,s,d,w,a,s,d,w,a,s,d,w,a,s,d,w,a,s,d,w,a,s,d,w,a,s,d,w,a,s,d,w,a,s,d,w,a,s,d,w,a,s,d,w,a,s,d,w,a,s,d,w,a,s,d,w,a,s,d,w,a,s,d,w,a,s,d,w,a,s,d,w,a,s,d)
   }
 }
--- src/test/scala/de/htwg/se/twothousandfortyeight/model/GameSpec.scala
+++ src/test/scala/de/htwg/se/twothousandfortyeight/model/GameSpec.scala
@@ -3,14 +3,15 @@ package de.htwg.se.twothousandfortyeight.model
 import de.htwg.se.twothousandfortyeight.TwoThousandFortyEight
 import de.htwg.se.twothousandfortyeight.model.gameModel.gameBaseImpl.Game
 import org.scalatest._
+import org.scalatest.matchers.should.Matchers
 
-class GameSpec extends wordspec.AnyWordSpec {
+class GameSpec extends wordspec.AnyWordSpec with Matchers {
   "A Game" when {
     "new" should {
       val game = new Game
       "have a reset method" in {
-        game.reset.score.value === 0
-        game.reset.grid.length.equals(TwoThousandFortyEight.FIELD_SIZE * TwoThousandFortyEight.FIELD_SIZE)
+        game.reset.score.value shouldBe 0
+        game.reset.grid.length shouldBe TwoThousandFortyEight.FIELD_SIZE * TwoThousandFortyEight.FIELD_SIZE
       }
       "have a rotate method" in {
         !game.rotate.grid.equals(game.grid)
@@ -19,22 +20,22 @@ class GameSpec extends wordspec.AnyWordSpec {
         game.rotate.rotate.rotate.rotate.grid.equals(game.grid)
       }
       "have a left method" in {
-        game.left.grid.length === 16
+        game.left.grid.length shouldBe 16
       }
       "have a right method" in {
-        game.right.grid.length === 16
+        game.right.grid.length shouldBe 16
       }
       "have a up method" in {
-        game.up.grid.length === 16
+        game.up.grid.length shouldBe 16
       }
       "have a down method" in {
-        game.down.grid.length === 16
+        game.down.grid.length shouldBe 16
       }
       "have a mergeSingleLine, moveSingleLine and getSingleLine method" in {
-        game.getSingleLine(0).moveSingleLine.mergeSingleLine.grid.length == 4
+        game.getSingleLine(0).moveSingleLine.mergeSingleLine.grid.length shouldBe 4
       }
       "have a toString method" in {
-        game.toString.length === 51
+        game.toString.length shouldBe 54
       }
     }
   }
