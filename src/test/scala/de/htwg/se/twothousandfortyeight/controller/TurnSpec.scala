5997d08ea1a4d14fcf8d26d757719b4a90f11e98
Philip Yannick Benischke <philip.benischke@gmail.com>
2020-11-16 11:31:55 +0100

    test: fixed tests

--- src/test/scala/de/htwg/se/twothousandfortyeight/controller/TurnSpec.scala
+++ src/test/scala/de/htwg/se/twothousandfortyeight/controller/TurnSpec.scala
@@ -9,7 +9,7 @@ class TurnSpec extends AnyWordSpec {
 
   "A turn" should {
     "have a turnLeft method" in {
-      assert(turn.turnLeft == 0)
+      assert(turn.turnLeft === 0)
     }
   }
 
@@ -18,31 +18,31 @@ class TurnSpec extends AnyWordSpec {
       val turn = new Turn
 
       "have a turnLeft method" in {
-        turn.turnLeft == 0
+        turn.turnLeft === 0
       }
       "have a turnRight method" in {
-        turn.turnRight == 0
+        turn.turnRight === 0
       }
       "have a turnDown method" in {
-        turn.turnDown == 0
+        turn.turnDown === 0
       }
       "have a turnUp method" in {
-        turn.turnUp == 0
+        turn.turnUp === 0
       }
       "have a turnUndo method" in {
-        turn.turnUndo == 0
+        turn.turnUndo === 0
       }
       "have a turnReset method" in {
-        turn.turnReset == 0
+        turn.turnReset === 0
       }
       "have a turnSave method" in {
-        turn.turnSave == 0
+        turn.turnSave === 0
       }
       "have a turnLoad method" in {
-        turn.turnLoad == 0
+        turn.turnLoad === 0
       }
       "have a evaluate method" in {
-        turn.evaluate == 0
+        turn.evaluate === 0
       }
       "have a way to win" in {
         val winArray = Array[Tile](new Tile(2), new Tile(1024), new Tile(1024),
@@ -50,8 +50,8 @@ class TurnSpec extends AnyWordSpec {
           new Tile(2), new Tile(2), new Tile(2), new Tile(2),
           new Tile(2), new Tile(2), new Tile(2), new Tile(2))
         turn.game = new Game(winArray)
-        turn.turnRight == 1
-        turn.evaluate == 1
+        turn.turnRight === 1
+        turn.evaluate === 1
       }
       "have a way to lose" in {
         val loseArray = Array[Tile](
@@ -60,8 +60,8 @@ class TurnSpec extends AnyWordSpec {
           new Tile(32), new Tile(64), new Tile(128), new Tile(256),
           new Tile(256), new Tile(128), new Tile(64), new Tile(32))
         turn.game = new Game(loseArray)
-        turn.turnRight == 2
-        turn.evaluate == 2
+        turn.turnRight === 2
+        turn.evaluate === 2
       }
     }
   }
