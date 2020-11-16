c709231b222a45de5e37b9a89f33c8560f5a38b3
Philip Yannick Benischke <philip.benischke@gmail.com>
2020-11-16 11:29:54 +0100

    test: fixed tests

--- src/test/scala/de/htwg/se/twothousandfortyeight/model/GameSpec.scala
+++ src/test/scala/de/htwg/se/twothousandfortyeight/model/GameSpec.scala
@@ -1,39 +1,41 @@
 package de.htwg.se.twothousandfortyeight.model
 
-import org.scalatest.wordspec.AnyWordSpec
+import de.htwg.se.twothousandfortyeight.TwoThousandFortyEight
+import de.htwg.se.twothousandfortyeight.model.gameModel.gameBaseImpl.Game
+import org.scalatest._
 
-class GameSpec extends AnyWordSpec {
-  /*"A Game" when {
+class GameSpec extends wordspec.AnyWordSpec {
+  "A Game" when {
     "new" should {
       val game = new Game
       "have a reset method" in {
-        game.reset.score.value should be(0)
-        game.reset.grid.length should be(TwoThousandFortyEight.FIELD_SIZE * TwoThousandFortyEight.FIELD_SIZE)
+        game.reset.score.value === 0
+        game.reset.grid.length.equals(TwoThousandFortyEight.FIELD_SIZE * TwoThousandFortyEight.FIELD_SIZE)
       }
       "have a rotate method" in {
-        game.rotate.grid should not be (game.grid)
-        game.rotate.rotate.grid should not be (game.grid)
-        game.rotate.rotate.rotate.grid should not be (game.grid)
-        game.rotate.rotate.rotate.rotate.grid should be(game.grid)
+        !game.rotate.grid.equals(game.grid)
+        !game.rotate.rotate.grid.equals(game.grid)
+        !game.rotate.rotate.rotate.grid.equals(game.grid)
+        game.rotate.rotate.rotate.rotate.grid.equals(game.grid)
       }
       "have a left method" in {
-        game.left.grid.length should be(16)
+        game.left.grid.length === 16
       }
       "have a right method" in {
-        game.right.grid.length should be(16)
+        game.right.grid.length === 16
       }
       "have a up method" in {
-        game.up.grid.length should be(16)
+        game.up.grid.length === 16
       }
       "have a down method" in {
-        game.down.grid.length should be(16)
+        game.down.grid.length === 16
       }
       "have a mergeSingleLine, moveSingleLine and getSingleLine method" in {
-        game.getSingleLine(0).moveSingleLine.mergeSingleLine.grid.length should be(4)
+        game.getSingleLine(0).moveSingleLine.mergeSingleLine.grid.length == 4
       }
       "have a toString method" in {
-        game.toString.length should be(51)
+        game.toString.length === 51
       }
     }
-  }*/
+  }
 }
--- src/test/scala/de/htwg/se/twothousandfortyeight/util/UtilsSpec.scala
+++ src/test/scala/de/htwg/se/twothousandfortyeight/util/UtilsSpec.scala
@@ -1,53 +1,57 @@
 package de.htwg.se.twothousandfortyeight.util
 
+import de.htwg.se.twothousandfortyeight.controller.turnBaseImpl.Turn
+import de.htwg.se.twothousandfortyeight.model.gameModel.gameBaseImpl.Tile
 import org.scalatest.wordspec.AnyWordSpec
 
+import scala.swing.Color
+
 class UtilsSpec extends AnyWordSpec {
-  /*"A Util" when {
+  "A Util" when {
     "used" should {
       "have a processAction method" in {
         val turn = new Turn
 
-        Utils.processAction(turn, "left") should be(0)
-        Utils.processAction(turn, "right") should be(0)
-        Utils.processAction(turn, "down") should be(0)
-        Utils.processAction(turn, "up") should be(0)
-        Utils.processAction(turn, "undo") should be(0)
-        Utils.processAction(turn, "reset") should be(0)
-        Utils.processAction(turn, "save") should be(0)
-        Utils.processAction(turn, "load") should be(0)
-        Utils.processAction(turn, "help") should be(3)
-        Utils.processAction(turn, "blank") should be(0)
+        Utils.processAction(turn, "left") ===0
+        Utils.processAction(turn, "right") ===0
+        Utils.processAction(turn, "down") ===0
+        Utils.processAction(turn, "up") ===0
+        Utils.processAction(turn, "undo") ===0
+        Utils.processAction(turn, "reset") ===0
+        Utils.processAction(turn, "save") ===0
+        Utils.processAction(turn, "load") ===0
+        Utils.processAction(turn, "help") ===3
+        Utils.processAction(turn, "blank") ===0
       }
       "have a help method" in {
-        Utils.help should be("----------------HELP----------------\n| W - up    | Q - undo | R - reset |\n| A - left  | Z - save | T - exit  |\n| S - down  | U - load |           |\n| D - right |          |           |\n------------------------------------\n")
+        Utils.help.equals("----------------HELP----------------\n| W - up    | Q - undo | R - reset |\n| A - left  | Z - save | T - exit  |\n| S - down  | U - load |           |\n| D - right |          |           |\n------------------------------------\n")
       }
       "have a getBackgroundColor method" in {
         val tile2 = new Tile(2)
-        Utils.getBackGroundColor(tile2) should be(new Color(0xfff835))
+        Utils.getBackGroundColor(tile2).equals(new Color(0xfff835))
         val tile4 = new Tile(4)
-        Utils.getBackGroundColor(tile4) should be(new Color(0xaed100))
+        Utils.getBackGroundColor(tile4).equals(new Color(0xaed100))
         val tile8 = new Tile(8)
-        Utils.getBackGroundColor(tile8) should be(new Color(0x7dd100))
+        Utils.getBackGroundColor(tile8).equals(new Color(0x7dd100))
         val tile16 = new Tile(16)
-        Utils.getBackGroundColor(tile16) should be(new Color(0x50d100))
+        Utils.getBackGroundColor(tile16).equals(new Color(0x50d100))
         val tile32 = new Tile(32)
-        Utils.getBackGroundColor(tile32) should be(new Color(0x00d1a0))
+        Utils.getBackGroundColor(tile32).equals(new Color(0x00d1a0))
         val tile64 = new Tile(64)
-        Utils.getBackGroundColor(tile64) should be(new Color(0x005ad1))
+        Utils.getBackGroundColor(tile64).equals(new Color(0x005ad1))
         val tile128 = new Tile(128)
-        Utils.getBackGroundColor(tile128) should be(new Color(0x4c00d1))
+        Utils.getBackGroundColor(tile128).equals(new Color(0x4c00d1))
         val tile256 = new Tile(256)
-        Utils.getBackGroundColor(tile256) should be(new Color(0xb500d1))
+        Utils.getBackGroundColor(tile256).equals(new Color(0xb500d1))
         val tile512 = new Tile(512)
-        Utils.getBackGroundColor(tile512) should be(new Color(0xd10099))
+        Utils.getBackGroundColor(tile512).equals(new Color(0xd10099))
         val tile1024 = new Tile(1024)
-        Utils.getBackGroundColor(tile1024) should be(new Color(0xd1005e))
+        Utils.getBackGroundColor(tile1024).equals(new Color(0xd1005e))
         val tile2048 = new Tile(2048)
-        Utils.getBackGroundColor(tile2048) should be(new Color(0xd10000))
+        Utils.getBackGroundColor(tile2048).equals(new Color(0xd10000))
         val tile0 = new Tile(0)
-        Utils.getBackGroundColor(tile0) should be(new Color(0x707070))
+        Utils.getBackGroundColor(tile0).equals(new Color(0x707070))
       }
     }
-  }*/
+  }
 }
