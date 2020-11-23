e1545ea387c8278d5d19c1a924ff6ff9a4a2d0a3
LukeCaruzo <schmidt.lucas@googlemail.com>
2020-11-23 12:03:48 +0100

    added tests for move actor and removed unused functionalities

--- src/main/scala/de/htwg/se/twothousandfortyeight/controller/actorInternalDslImpl/Player.scala
+++ src/main/scala/de/htwg/se/twothousandfortyeight/controller/actorInternalDslImpl/Player.scala
@@ -1,7 +1,7 @@
 package de.htwg.se.twothousandfortyeight.controller.actorInternalDslImpl
 
 class Player {
-  val moveActor = new MoveActor
+  val moveActor = new MoveActor("playerActor")
 
   def Move(seq: String*): Unit = {
     seq.foreach(e => moveActor.move(e))
---
+++ src/test/scala/de/htwg/se/twothousandfortyeight/controller/MoveActorSpec.scala
@@ -0,0 +1,27 @@
+package de.htwg.se.twothousandfortyeight.controller
+
+import de.htwg.se.twothousandfortyeight.controller.actorInternalDslImpl.MoveActor
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatest.matchers.should.Matchers
+
+
+class MoveActorSpec extends AnyWordSpec with Matchers {
+  "A MoveActor" when {
+    "new" should {
+      val moveActor = new MoveActor("testActor")
+
+      "have a printTui method" in {
+        moveActor.printTui.length should be(69)
+      }
+      "have a printWin method" in {
+        moveActor.printWin.length should be(79)
+      }
+      "have a printLose method" in {
+        moveActor.printLose.length should be(80)
+      }
+      "have a printHelp method" in {
+        moveActor.printHelp.length should be(228)
+      }
+    }
+  }
+}
