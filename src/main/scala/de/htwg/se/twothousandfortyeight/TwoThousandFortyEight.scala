a132bed95fa8705168a8197c8bc04bdf442ba562
LukeCaruzo <schmidt.lucas@googlemail.com>
2020-12-05 16:04:07 +0100

    fixed build

--- src/main/scala/de/htwg/se/twothousandfortyeight/TwoThousandFortyEight.scala
+++ src/main/scala/de/htwg/se/twothousandfortyeight/TwoThousandFortyEight.scala
@@ -1,7 +1,9 @@
 package de.htwg.se.twothousandfortyeight
 
 import de.htwg.se.twothousandfortyeight.controller.turnBaseImpl.Turn
-import de.htwg.se.twothousandfortyeight.view.tui.{Rest, Tui}
+import de.htwg.se.twothousandfortyeight.view.rest.Rest
+import de.htwg.se.twothousandfortyeight.view.tui.Tui
+
 
 object TwoThousandFortyEight {
   val FIELD_SIZE = 4 // Tests and Slick are configured for 4
