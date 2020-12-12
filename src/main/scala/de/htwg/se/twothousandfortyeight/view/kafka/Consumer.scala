e83ca13c4fa99ed46ed455e46cede79f590ccb96
LukeCaruzo <schmidt.lucas@googlemail.com>
2020-12-12 13:53:50 +0100

    modified kafka and added docker yml

--- src/main/scala/de/htwg/se/twothousandfortyeight/view/kafka/Consumer.scala
+++ src/main/scala/de/htwg/se/twothousandfortyeight/view/kafka/Consumer.scala
@@ -1,5 +1,9 @@
 package de.htwg.se.twothousandfortyeight.view.kafka
 
+import akka.actor.Props
+import de.htwg.se.twothousandfortyeight.controller.actorBaseImpl.{CommandActor, TurnAsInstance}
+import de.htwg.se.twothousandfortyeight.controller.turnBaseImpl.Turn
+import de.htwg.se.twothousandfortyeight.view.streams.Streams.{singleStream, system}
 import org.apache.kafka.clients.consumer.KafkaConsumer
 
 import java.util
@@ -7,7 +11,6 @@ import java.util.Properties
 import scala.collection.JavaConverters._
 
 object Consumer extends App {
-
   val props = new Properties()
   props.put("bootstrap.servers", "localhost:9092")
   props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
@@ -15,15 +18,20 @@ object Consumer extends App {
   props.put("group.id", "something")
 
   val consumer = new KafkaConsumer[String, String](props)
-  val TOPIC = "test"
+  val TOPIC = "move"
 
   consumer.subscribe(util.Collections.singletonList(TOPIC))
 
+  val turn = new Turn
+  val turnAsInstance: TurnAsInstance = new TurnAsInstance(turn)
+  val cmdActor = system.actorOf(Props(classOf[CommandActor], turnAsInstance.turn))
+
   while (true) {
-    println("Polling..")
-    val records = consumer.poll(100)
+    println("Polling ...")
+    val records = consumer.poll(500)
+
     for (record <- records.asScala) {
-      println(record)
+      singleStream(cmdActor, turn, record.value(), true)
     }
   }
 }
--- src/main/scala/de/htwg/se/twothousandfortyeight/view/kafka/Producer.scala
+++ src/main/scala/de/htwg/se/twothousandfortyeight/view/kafka/Producer.scala
@@ -1,11 +1,11 @@
 package de.htwg.se.twothousandfortyeight.view.kafka
 
+import de.htwg.se.twothousandfortyeight.view.streams.Streams
 import org.apache.kafka.clients.producer._
 
 import java.util.Properties
 
 object Producer extends App {
-
   val props = new Properties()
   props.put("bootstrap.servers", "localhost:9092")
   props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
@@ -13,19 +13,15 @@ object Producer extends App {
 
   val producer = new KafkaProducer[String, String](props)
 
-  val TOPIC = "test"
+  val TOPIC = "move"
   val KEY = "message"
 
-  for (i <- 1 to 50) {
-    val record = new ProducerRecord(TOPIC, KEY, s"hello $i")
-    Thread.sleep(1000)
-    println("Sending ..")
+  for (i <- 1 to 100) {
+    val record = new ProducerRecord(TOPIC, KEY, Streams.randomMoveSimple)
+    Thread.sleep(2000)
+    println("Sending ...")
     producer.send(record)
   }
-
-  val record = new ProducerRecord(TOPIC, KEY, "the end " + new java.util.Date)
-  println("Sending final message..")
-  producer.send(record)
 
   producer.close()
 }
