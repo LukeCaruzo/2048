60af4506c61f6e44d36c9d40fbce8ecfe02b64ef
Philip Yannick Benischke <philip.benischke@gmail.com>
2021-01-09 15:55:55 +0100

    refactor: Utils.scala methods in pyhton style

--- src/main/scala/de/htwg/se/twothousandfortyeight/util/Utils.scala
+++ src/main/scala/de/htwg/se/twothousandfortyeight/util/Utils.scala
@@ -4,7 +4,7 @@ import de.htwg.se.twothousandfortyeight.controller.TurnTrait
 import de.htwg.se.twothousandfortyeight.model.gameModel.TileTrait
 
 object Utils:
-  def processAction(turn: TurnTrait, action: String): Int = {
+  def processAction(turn: TurnTrait, action: String): Int =
     action match {
       case "left" => turn.turnLeft
       case "right" => turn.turnRight
@@ -18,19 +18,12 @@ object Utils:
       case "help" => return 3
       case _ => turn.evaluate
     }
-  }
 
-  def help: String = {
-    val newline = "\n"
-    val sb = new StringBuilder
-
-    sb.append("----------------HELP----------------" + newline)
-    sb.append("| W - up    | Q - undo | R - reset |" + newline)
-    sb.append("| A - left  | Z - save | T - exit  |" + newline)
-    sb.append("| S - down  | U - load |           |" + newline)
-    sb.append("| D - right |          |           |" + newline)
-    sb.append("------------------------------------" + newline)
-
-    return sb.toString
-  }
-
+  def help: String =
+    """----------------HELP----------------
+      || W - up    | Q - undo | R - reset |
+      || A - left  | Z - save | T - exit  |
+      || S - down  | U - load |           |
+      || D - right |          |           |
+      |------------------------------------
+      |""".stripMargin
