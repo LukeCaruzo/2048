b20a27a9de7853c40f7927a61fcfb974e71572c4
Philip Yannick Benischke <philip.benischke@gmail.com>
2021-01-17 12:24:51 +0100

    refactor: cleaned up process input in Tui.scala

--- src/main/scala/de/htwg/se/twothousandfortyeight/view/tui/Tui.scala
+++ src/main/scala/de/htwg/se/twothousandfortyeight/view/tui/Tui.scala
@@ -27,17 +27,17 @@ class Tui(turn: TurnTrait) {
 
   def processInput(line: String): String = {
     line.charAt(0) match {
-      case 'a' => return "left"
-      case 'd' => return "right"
-      case 's' => return "down"
-      case 'w' => return "up"
-      case 'q' => return "undo"
-      case 'r' => return "reset"
-      case 'z' => return "save"
-      case 'u' => return "load"
-      case 't' => return "exit"
-      case 'h' => return "help"
-      case _ => return "blank"
+      case 'a' => "left"
+      case 'd' => "right"
+      case 's' => "down"
+      case 'w' => "up"
+      case 'q' => "undo"
+      case 'r' => "reset"
+      case 'z' => "save"
+      case 'u' => "load"
+      case 't' => "exit"
+      case 'h' => "help"
+      case _ => "blank"
     }
   }
 
